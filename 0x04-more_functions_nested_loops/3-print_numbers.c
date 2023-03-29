62919913ce1cd2404d8f2c3292cb054fa479c816
Empressteepha <pinkflannelng@gmail.com>
2023-03-29 09:20:28 -0700

    had to copy the _putchar file into this directory

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_number - prints numbers from 0 to 9
+ * return: void
+ */
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c<='9'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+}
