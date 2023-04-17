3555e34ed695352222c25828ca97f812c7b12685
Empressteepha <pinkflannelng@gmail.com>
2023-04-17 03:01:51 -0700

    printing factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,6 +7,7 @@
  *
  * Return: factorial of n
  */
+
 int factorial(int n)
 {
 	if (n < 0)
@@ -14,5 +15,5 @@ int factorial(int n)
 	if (n == 0)
 		return (1);
 	else
-		return (n * factorial(n -1));
+		return (n *factorial(n -1));
 }
