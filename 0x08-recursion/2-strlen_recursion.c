6b2641e6adde0451001605faf944919efff4dac1
Empressteepha <pinkflannelng@gmail.com>
2023-06-08 10:53:26 -0700

    commit no 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,7 +3,7 @@
 /**
  * _strlen_recursion - outputs length of strings
  * @s: the string to be measyred
- * return: length of string
+ * Return: length of string
  */
 
 int _strlen_recursion(char *s)
