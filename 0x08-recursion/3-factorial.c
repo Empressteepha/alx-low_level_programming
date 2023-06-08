b3b3e414db6529e525db74157d236a3eb6f368f9
Empressteepha <pinkflannelng@gmail.com>
2023-06-08 10:51:30 -0700

    factorial of n too

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,14 +2,14 @@
 /**
  * factorial - returns the factorial value of num
  * @n: number whose factorial
- * return: n factorial
+ * Return: n factorial
  */
 
 int factorial(int n)
 {
 	if (n < 0)
-		return(-1);
+		return (-1);
 	if (n == 0)
-		return(1);
-	return(n * factorial(n - 1));
+		return (1);
+	return (n * factorial(n - 1));
 }
